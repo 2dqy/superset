122d4aac98fedf036cbac5efe83fbbc26d328951
2dqy <info@2dqy.com>
2025-05-13 18:22:27 +0800

    Update constants.py

--- superset/constants.py
+++ superset/constants.py
@@ -27,7 +27,7 @@ DEFAULT_USER_AGENT = "Apache Superset"
 NULL_STRING = "<NULL>"
 EMPTY_STRING = "<empty string>"
 
-CHANGE_ME_SECRET_KEY = "CHANGE_ME_TO_A_COMPLEX_RANDOM_SECRET"  # noqa: S105
+CHANGE_ME_SECRET_KEY = "Zg1O6xjQfA9hckQqKeHilHfkQUyubkmTGL0Vk1z/eBnY0BZ0lJjMCGb0"  # noqa: S105
 
 # UUID for the examples database
 EXAMPLES_DB_UUID = "a2dc77af-e654-49bb-b321-40f6b559a1ee"
